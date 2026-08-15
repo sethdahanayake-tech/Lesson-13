516f20b24c0b14bc2c4c825ea1a9edab0a7526d9
sethdahanayake-tech <seth.dahanayake@gmail.com>
2026-08-16 11:13:18 +1200

    Add files via upload

---
+++ Angletriangle.py
@@ -0,0 +1,12 @@
+#Write a program to demonstrate a right angle triangle pattern?
+#Take input
+print("Half Pyramid Pattern of Stars")
+n = int(input("enter the number of rows: "))
+#outer loop to handle number of rows
+for i in range(n):
+    #inner loop to handle number of columns
+    for j in range(i+1):
+        #display result
+        print("*", end="")
+    print()
+
